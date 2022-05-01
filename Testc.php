8ac6a05503df3f5c906b2f97a55b5ae9b7a1f68a
Partha <softaweb13@gmail.com>
2022-05-01 17:42:57 +0530

    ggg

--- Testc.php
+++ Testc.php
@@ -4,17 +4,50 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Student;
+
 class Testc extends Controller
 {
-    public function demo(Request $r){
-        
-        $key=$r->key;
-       
-        $arr=array(
-         'msg'=>'I am Laravel'
-        );
-        echo json_encode($arr);
-    
-   
-    }
+   public function home(){
+
+  $arr=array(
+
+  'msg'=>'I am a react laravel developer'
+  );
+
+  echo json_encode($arr);
+
+   }
+
+
+   public function insstd(Request $r){
+
+
+      $fl=$r->file("pimg");
+      if(isset($fl)){
+         $fn=time().$fl->getClientOriginalName();
+         $fl->move("user_img",$fn);
+      }
+
+      $obj=new Student();
+      $obj->name=$r->name;
+      $obj->email=$r->email;
+      $obj->gender=$r->gender;
+      $obj->stream=$r->stream;
+      $obj->subject=$r->subject;
+      if(isset($fn)){
+         $obj->pimg=$fn;
+      }else{
+         $obj->pimg="default.jpg";
+      }
+      $obj->save();
+      
+       $arr=array(
+
+  'msg'=>'Student inserted'
+  );
+
+  echo json_encode($arr);
+
+   }
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,9 +4,20 @@ function Home(){
     let [email,setEmail]=useState("");
     let [gender,setGender]=useState("");
     let [stream,setStream]=useState("");
+    let [subject,setSubject]=useState([]);
+
+    let [pimg,setPimg]=useState(null);
 
     function setg(ev){
         setGender(ev.target.value);
+    }
+    function handleChangeChk(ev){
+        if(ev.target.checked==true){
+        subject.push(ev.target.value);
+        }else{
+            var pos=subject.indexOf(ev.target.value);
+            subject.splice(pos,1);
+        }
     }
    
     return (
@@ -32,13 +43,28 @@ function Home(){
          <option value="ECE">ECE</option>
          </select></p>
 
+         <p>Subject</p>
+         <p><input type="checkbox" value="C" onChange={handleChangeChk}/> C</p>
+         <p><input type="checkbox" value="C++" onChange={handleChangeChk}/> C++</p>
+         <p><input type="checkbox" value="Java" onChange={handleChangeChk}/> Java</p>
+
+         <p>Image</p>
+         <input type="file"  onChange={(ev)=>{
+          setPimg(ev.target.files[0]);
+         }} />
+          
          <p><input type="button" value="Save" onClick={async ()=>{
+
+       
             var fd=new FormData();
             fd.append("name",name);
             fd.append("email",email);
             fd.append("gender",gender);
             fd.append("stream",stream);
-            var resp=await fetch("http://localhost/talara/api/customarin",{
+            fd.append("subject",subject);
+            fd.append("pimg",pimg);
+
+            var resp=await fetch("http://localhost/talara/api/stdins",{
                 method:'POST',
                 body:fd
             });
